3d03f8e7d44d95f48a0069296a2254eb7f317b70
Iqbal Ishak <mhd.iqbalishak@gmail.com>
2024-12-25 14:18:36 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Getstarted(), // Replace with your initial screen
+      home: HomePage(), // Replace with your initial screen
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.karlaTextTheme(
